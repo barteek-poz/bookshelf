2d37b4c46f834e9a842013fa94a2db0d85792eef
barteek-poz <szowior4@wp.pl>
2025-08-19 23:22:57 +0200

    add editbook tests

--- frontend/src/pages/Dashboard/Dashboard.test.tsx
+++ frontend/src/pages/Dashboard/Dashboard.test.tsx
@@ -6,111 +6,10 @@ import Dashboard from "./Dashboard";
 import useFetch from "../../hooks/useFetch";
 import ErrorPage from "../ErrorPage/ErrorPage";
 
-jest.mock("../../hooks/useFetch", ()=> ({
+jest.mock("../../hooks/useFetch", () => ({
   __esModule: true,
-  default: jest.fn()
-}))
-
-
-  test("renders books", async () => {
-    (useFetch as jest.Mock).mockReturnValue({
-    data: [
-              {
-                id: 1,
-                author: "Test",
-                title: "Title",
-                createdBy: 1,
-                coverUrl: "coverUrl",
-                genre: "genre",
-                publishYear: "publishYear",
-              },
-            ],
-    isPending: false, 
-    error: null
-    })
-    render(
-      <MemoryRouter>
-        <AuthContext.Provider
-          value={{
-            user: { id: 1, is_admin: false },
-            setAccessToken: jest.fn(),
-            setIsAuthenticated: jest.fn(),
-            setUser: jest.fn(),
-            isAuthenticated: true,
-            accessToken: "accessToken",
-            loading: false,
-          }}>
-          <ErrorProvider>
-            <Dashboard />
-          </ErrorProvider>
-        </AuthContext.Provider>
-      </MemoryRouter>
-    );
-    await waitFor(() => {
-      expect(screen.getByAltText("shelf-img")).toBeInTheDocument();
-      expect(screen.getByRole("link")).toHaveAttribute("href", "/books/1");
-    });
-  });
-
-
-  test("renders message when no books in library", async () => {
-    (useFetch as jest.Mock).mockReturnValue({
-    data: [],
-    isPending: false, 
-    error: null
-    })
-    render(
-      <MemoryRouter>
-        <AuthContext.Provider
-          value={{
-            user: { id: 1, is_admin: false },
-            setAccessToken: jest.fn(),
-            setIsAuthenticated: jest.fn(),
-            setUser: jest.fn(),
-            isAuthenticated: true,
-            accessToken: "accessToken",
-            loading: false,
-          }}>
-          <ErrorProvider>
-            <Dashboard />
-          </ErrorProvider>
-        </AuthContext.Provider>
-      </MemoryRouter>
-    );
-    await waitFor(() => {
-      expect(screen.getByText("You don't have any book in your library.")).toBeInTheDocument()
-    });
-  });
-
-
-
-test("rendering loader when pending equals true", () => {
-  (useFetch as jest.Mock).mockReturnValue({
-    data: null,
-    isPending: true, 
-    error: null
-  })
-  render(
-      <MemoryRouter>
-        <AuthContext.Provider
-          value={{
-            user: { id: 1, is_admin: false },
-            setAccessToken: jest.fn(),
-            setIsAuthenticated: jest.fn(),
-            setUser: jest.fn(),
-            isAuthenticated: true,
-            accessToken: "accessToken",
-            loading: false,
-          }}>
-          <ErrorProvider>
-            <Dashboard />
-          </ErrorProvider>
-        </AuthContext.Provider>
-      </MemoryRouter>
-    );
-
-  expect(screen.getByText("Loading...")).toBeInTheDocument()
-})
+  default: jest.fn(),
+}));
 
 const mockNavigate = jest.fn();
 jest.mock("react-router-dom", () => ({
@@ -118,38 +17,136 @@ jest.mock("react-router-dom", () => ({
   useNavigate: () => mockNavigate,
 }));
 
+test("renders books", async () => {
+  (useFetch as jest.Mock).mockReturnValue({
+    data: [
+      {
+        id: 1,
+        author: "Test",
+        title: "Title",
+        createdBy: 1,
+        coverUrl: "coverUrl",
+        genre: "genre",
+        publishYear: "publishYear",
+      },
+    ],
+    isPending: false,
+    error: null,
+  });
+  render(
+    <MemoryRouter>
+      <AuthContext.Provider
+        value={{
+          user: { id: 1, is_admin: false },
+          setAccessToken: jest.fn(),
+          setIsAuthenticated: jest.fn(),
+          setUser: jest.fn(),
+          isAuthenticated: true,
+          accessToken: "accessToken",
+          loading: false,
+        }}
+      >
+        <ErrorProvider>
+          <Dashboard />
+        </ErrorProvider>
+      </AuthContext.Provider>
+    </MemoryRouter>
+  );
+  await waitFor(() => {
+    expect(screen.getByAltText("shelf-img")).toBeInTheDocument();
+    expect(screen.getByRole("link")).toHaveAttribute("href", "/books/1");
+  });
+});
 
+test("renders message when no books in library", async () => {
+  (useFetch as jest.Mock).mockReturnValue({
+    data: [],
+    isPending: false,
+    error: null,
+  });
+  render(
+    <MemoryRouter>
+      <AuthContext.Provider
+        value={{
+          user: { id: 1, is_admin: false },
+          setAccessToken: jest.fn(),
+          setIsAuthenticated: jest.fn(),
+          setUser: jest.fn(),
+          isAuthenticated: true,
+          accessToken: "accessToken",
+          loading: false,
+        }}
+      >
+        <ErrorProvider>
+          <Dashboard />
+        </ErrorProvider>
+      </AuthContext.Provider>
+    </MemoryRouter>
+  );
+  await waitFor(() => {
+    expect(screen.getByText("You don't have any book in your library.")).toBeInTheDocument();
+  });
+});
 
-test("rendering error message when error", async() => {
+test("rendering loader when pending equals true", () => {
   (useFetch as jest.Mock).mockReturnValue({
     data: null,
-    isPending: false, 
-    error: true
-  })
+    isPending: true,
+    error: null,
+  });
   render(
-      <MemoryRouter initialEntries={["/"]}>
-        <AuthContext.Provider
-          value={{
-            user: { id: 1, is_admin: false },
-            setAccessToken: jest.fn(),
-            setIsAuthenticated: jest.fn(),
-            setUser: jest.fn(),
-            isAuthenticated: true,
-            accessToken: "accessToken",
-            loading: false,
-          }}>
-          <ErrorProvider>
-            <Routes>
-              <Route path="/" element={<Dashboard/>} />
-              <Route path="/error" element={<ErrorPage/>} />
-            </Routes>
-          </ErrorProvider>
-        </AuthContext.Provider>
-      </MemoryRouter>
-    );
+    <MemoryRouter>
+      <AuthContext.Provider
+        value={{
+          user: { id: 1, is_admin: false },
+          setAccessToken: jest.fn(),
+          setIsAuthenticated: jest.fn(),
+          setUser: jest.fn(),
+          isAuthenticated: true,
+          accessToken: "accessToken",
+          loading: false,
+        }}
+      >
+        <ErrorProvider>
+          <Dashboard />
+        </ErrorProvider>
+      </AuthContext.Provider>
+    </MemoryRouter>
+  );
+
+  expect(screen.getByText("Loading...")).toBeInTheDocument();
+});
+
+test("rendering error message when error", async () => {
+  (useFetch as jest.Mock).mockReturnValue({
+    data: null,
+    isPending: false,
+    error: true,
+  });
+  render(
+    <MemoryRouter initialEntries={["/"]}>
+      <AuthContext.Provider
+        value={{
+          user: { id: 1, is_admin: false },
+          setAccessToken: jest.fn(),
+          setIsAuthenticated: jest.fn(),
+          setUser: jest.fn(),
+          isAuthenticated: true,
+          accessToken: "accessToken",
+          loading: false,
+        }}
+      >
+        <ErrorProvider>
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/error" element={<ErrorPage />} />
+          </Routes>
+        </ErrorProvider>
+      </AuthContext.Provider>
+    </MemoryRouter>
+  );
 
   await waitFor(() => {
-    expect(mockNavigate).toHaveBeenCalledWith("/error")
-  })
-
-})
+    expect(mockNavigate).toHaveBeenCalledWith("/error");
+  });
+});
--- frontend/src/pages/EditBook/EditBook.test.tsx
+++ frontend/src/pages/EditBook/EditBook.test.tsx
@@ -1,14 +1,16 @@
 import { render, screen, waitFor } from "@testing-library/react";
-import { MemoryRouter } from "react-router";
+import { MemoryRouter, Route, Routes } from "react-router";
 import { AuthContext } from "../../context/AuthContext";
 import { ErrorProvider } from "../../context/ErrorContext";
 import useFetch from "../../hooks/useFetch";
 import EditBook from "./EditBook";
+import userEvent from "@testing-library/user-event";
+import BookPage from "../BookPage/BookPage";
 
-jest.mock("../../components/GenreSelect/GenreSelect", () => ({ control, defaultValue }: {control: any; defaultValue: string | null}) => {
+jest.mock("../../components/GenreSelect/GenreSelect", () => ({ control, defaultValue }: { control: any; defaultValue: string | null }) => {
   return (
     <select data-testid="genre-select" defaultValue={defaultValue} {...control}>
-      {defaultValue && <option value={defaultValue}>{defaultValue}</option> }
+      {defaultValue && <option value={defaultValue}>{defaultValue}</option>}
       <option value="fantasy">Fantasy</option>
       <option value="sci-fi">Sci-Fi</option>
     </select>
@@ -16,24 +18,30 @@ jest.mock("../../components/GenreSelect/GenreSelect", () => ({ control, defaultV
 });
 
 jest.mock("../../hooks/useFetch", () => ({
-    __esModule: true,
-    default: jest.fn()
-}))
+  __esModule: true,
+  default: jest.fn(),
+}));
+
+const mockNavigate = jest.fn();
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useNavigate: () => mockNavigate,
+}));
 
 test("form render", async () => {
-   (useFetch as jest.Mock).mockReturnValue({
+  (useFetch as jest.Mock).mockReturnValue({
     data: {
-            id: 1,
-            author: "Test author",
-            title: "Test title",
-            createdBy: 1,
-            coverUrl: "coverUrl",
-            genre: "Test genre",
-            publishYear: "Test year",
-              },
-    isPending: false, 
-    error: null
-    })
+      id: 1,
+      author: "Test author",
+      title: "Test title",
+      createdBy: 1,
+      coverUrl: "coverUrl",
+      genre: "Test genre",
+      publishYear: "Test year",
+    },
+    isPending: false,
+    error: null,
+  });
   render(
     <MemoryRouter>
       <AuthContext.Provider
@@ -54,18 +62,87 @@ test("form render", async () => {
     </MemoryRouter>
   );
   expect(screen.getByAltText("book cover")).toHaveAttribute("src", "coverUrl");
-  expect(screen.getByLabelText("Select book cover")).toBeInTheDocument()
-  expect(screen.getByDisplayValue("Test title")).toBeInTheDocument()
-  expect(screen.getByDisplayValue("Test author")).toBeInTheDocument()
-  expect(screen.getByDisplayValue("Test year")).toBeInTheDocument()
-  const genreSelect = screen.getByTestId("genre-select") as HTMLSelectElement
-  expect(genreSelect.value).toBe("Test genre")
-  expect(screen.getByRole("button", {name: "Save"})).toBeInTheDocument()
-  expect(screen.getByRole("button", {name: "Cancel"})).toBeInTheDocument()
+  expect(screen.getByLabelText("Select book cover")).toBeInTheDocument();
+  expect(screen.getByDisplayValue("Test title")).toBeInTheDocument();
+  expect(screen.getByDisplayValue("Test author")).toBeInTheDocument();
+  expect(screen.getByDisplayValue("Test year")).toBeInTheDocument();
+  const genreSelect = screen.getByTestId("genre-select") as HTMLSelectElement;
+  expect(genreSelect.value).toBe("Test genre");
+  expect(screen.getByRole("button", { name: "Save" })).toBeInTheDocument();
+  expect(screen.getByRole("button", { name: "Cancel" })).toBeInTheDocument();
 });
 
 describe("buttons behaviour", () => {
-    test("submit new book data", () => {
-        
-    })
-})
+   beforeEach(() => {
+      (globalThis as any).fetch = jest.fn().mockResolvedValue({
+        ok: true,
+        json: async () => ({}),
+      });
+    });
+  test("submit new book data", async () => {
+    (useFetch as jest.Mock).mockReturnValue({
+      data: {
+        id: 1,
+        author: "Test author",
+        title: "Test title",
+        createdBy: 1,
+        coverUrl: "coverUrl",
+        genre: "Test genre",
+        publishYear: "Test year",
+      },
+      isPending: false,
+      error: null,
+    });
+   
+    render(
+      <MemoryRouter initialEntries={["/books/1/edit"]}>
+        <AuthContext.Provider
+          value={{
+            user: { id: 1, is_admin: false },
+            setAccessToken: jest.fn(),
+            setIsAuthenticated: jest.fn(),
+            setUser: jest.fn(),
+            isAuthenticated: true,
+            accessToken: "accessToken",
+            loading: false,
+          }}
+        >
+          <ErrorProvider>
+            <Routes>
+              <Route path="/books/:id/edit" element={<EditBook />} />
+              <Route path="/books/:id" element={<BookPage />} />
+            </Routes>
+          </ErrorProvider>
+        </AuthContext.Provider>
+      </MemoryRouter>
+    );
+    const bookCover = screen.getByAltText("book cover");
+    const coverBtn = screen.getByLabelText("Select book cover");
+    const titleInput = screen.getByDisplayValue("Test title");
+    const authorInput = screen.getByDisplayValue("Test author");
+    const yearInput = screen.getByDisplayValue("Test year");
+    const genreSelect = screen.getByTestId("genre-select") as HTMLSelectElement;
+    const saveBtn = screen.getByRole("button", { name: "Save" });
+
+    const mockFile = new File(["new moc cover img"], "new-cover.png", {
+      type: "image/png",
+    });
+
+    await userEvent.clear(titleInput);
+    await userEvent.type(titleInput, "New title");
+    await userEvent.clear(authorInput)
+    await userEvent.type(authorInput, "New author");
+    await userEvent.clear(yearInput)
+    await userEvent.type(yearInput, "2020");
+    await userEvent.upload(coverBtn, mockFile);
+    await userEvent.selectOptions(genreSelect, "fantasy")
+    await userEvent.click(saveBtn);
+    await waitFor(() => {
+      expect(mockNavigate).toHaveBeenCalledWith("/books/1");
+      expect(screen.getByDisplayValue("New title")).toBeInTheDocument();
+      expect(screen.getByDisplayValue("New author")).toBeInTheDocument();
+      expect(screen.getByDisplayValue("2020")).toBeInTheDocument();
+      expect(screen.getByDisplayValue("Fantasy")).toBeInTheDocument();
+    });
+  });
+});
