b57a59956f9fd95b1de28fcf94ea49fa19ff3dde
barteek-poz <szowior4@wp.pl>
2025-08-29 17:32:41 +0200

    fix ts types p4

--- backend/models/adminModel.ts
+++ backend/models/adminModel.ts
@@ -1,13 +1,13 @@
-import { RowDataPacket } from "mysql2";
+import mysql, { RowDataPacket } from "mysql2/promise";
 import { pool } from "../server";
 import { AdminSummary } from "../types/adminTypes";
 
 export const getAdminSummaryModel = async (): Promise<AdminSummary> => {
-    const [rows] = await pool.query<AdminSummary[] & RowDataPacket[]>(
-      `SELECT 
+  const [rows] = await pool.query<AdminSummary[] & RowDataPacket[]>(
+    `SELECT 
         (SELECT COUNT(*) FROM books) AS numOfBooks,
         (SELECT COUNT(*) FROM users) AS numOfUsers`
-    );
-  
-    return rows[0]; // ponieważ zapytanie zwraca jedną linię
-  };
+  );
+
+  return rows[0]; // returns 1 line
+};
--- backend/server.ts
+++ backend/server.ts
@@ -1,38 +1,33 @@
-import dotenv from 'dotenv'
-import http from 'http'
-import mysql from 'mysql2'
-import type { Pool } from "mysql2/promise"
-import app from "./app"
+import dotenv from "dotenv";
+import http from "http";
+import mysql from "mysql2/promise";
+import type { Pool } from "mysql2/promise";
+import app from "./app";
 
-// const options = { 
+// const options = {
 //     key: fs.readFileSync('./cert/localhost-key.pem'),
 //     cert: fs.readFileSync('./cert/localhost.pem')
 // }
 
-dotenv.config({path:'.env'})
+dotenv.config({ path: ".env" });
 
-export const pool:Pool = mysql.createPool({
-    host: process.env.MYSQL_HOST || 'localhost',
-    port: process.env.MYSQL_PORT ? parseInt(process.env.MYSQL_PORT) : 8000,
-    user: process.env.MYSQL_USER, 
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-    ssl: {
-        // ca:fs.readFileSync('./cert/ca.pem'),
-        rejectUnauthorized: true
-    }
-}).promise()
+export const pool: Pool = mysql.createPool({
+  host: process.env.MYSQL_HOST || "localhost",
+  port: process.env.MYSQL_PORT ? parseInt(process.env.MYSQL_PORT) : 8000,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  ssl: {
+    // ca:fs.readFileSync('./cert/ca.pem'),
+    rejectUnauthorized: true,
+  },
+});
 
-
-
-if(!process.env.DATABASE || !process.env.DATABASE_PASSWORD) {
-    throw new Error('Missing database credentials in enviromental variables')
+if (!process.env.DATABASE || !process.env.DATABASE_PASSWORD) {
+  throw new Error("Missing database credentials in enviromental variables");
 }
-const DB = process.env.DATABASE.replace('<db_password>', process.env.DATABASE_PASSWORD)
+const DB = process.env.DATABASE.replace("<db_password>", process.env.DATABASE_PASSWORD);
 
-const port = process.env.PORT
+const port = process.env.PORT;
 const server = http.createServer(app);
 app.listen(port, () => console.log(`Server running on port ${port}`));
-
-
-
