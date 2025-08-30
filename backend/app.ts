972c54c1d4c7d17e21abf2c60867aae87c344237
barteek-poz <szowior4@wp.pl>
2025-08-30 14:31:15 +0200

    fix prod url 2

--- backend/app.ts
+++ backend/app.ts
@@ -1,29 +1,39 @@
-import express, { Application } from 'express'
-import cors, {CorsOptions} from 'cors'
-import cookieParser from 'cookie-parser'
-import booksRouter from './routes/booksRoutes'
-import genresRouter from './routes/genresRoutes'
-import usersRouter from './routes/userRoutes'
-import authRouter from './routes/authRoutes'
-import adminRouter from './routes/adminRoutes'
+import express, { Application } from "express";
+import cors, { CorsOptions } from "cors";
+import cookieParser from "cookie-parser";
+import booksRouter from "./routes/booksRoutes";
+import genresRouter from "./routes/genresRoutes";
+import usersRouter from "./routes/userRoutes";
+import authRouter from "./routes/authRoutes";
+import adminRouter from "./routes/adminRoutes";
+const path = require("path");
 
-const corsOptions:CorsOptions = {
-    origin: 'https://bookshelf-nou0.onrender.com',  
-    credentials: true,  
-    allowedHeaders: ['Content-Type', 'Authorization'], 
-    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
-  };
 
-const app:Application = express()
-app.use(cors(corsOptions))
-app.use(express.json())
-app.use(cookieParser())
+
+const corsOptions: CorsOptions = {
+  origin: "https://bookshelf-nou0.onrender.com",
+  credentials: true,
+  allowedHeaders: ["Content-Type", "Authorization"],
+  methods: ["GET", "POST", "PATCH", "DELETE"],
+};
+
+const app: Application = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/v1/books', booksRouter)
-app.use('/api/v1/genres', genresRouter)
-app.use('/api/v1/users', usersRouter)
-app.use('/api/v1/auth', authRouter)
-app.use('/api/v1/admin', adminRouter)
+app.use("/api/v1/books", booksRouter);
+app.use("/api/v1/genres", genresRouter);
+app.use("/api/v1/users", usersRouter);
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/admin", adminRouter);
 
-export default app
+export default app;
