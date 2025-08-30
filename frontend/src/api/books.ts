39221502275afce92da78f71632a9962bc4e22eb
barteek-poz <szowior4@wp.pl>
2025-08-30 18:35:25 +0200

    fix add book fn

--- frontend/src/api/books.ts
+++ frontend/src/api/books.ts
@@ -1,21 +1,18 @@
-export const addExistingBookHandler = async (userId:number, accessToken:string, bookId:string) => {
-    try {
-      const response = await fetch(
-        `http://localhost:3000/api/v1/users/${userId}/add-book`,
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${accessToken}`,
-          },
-          credentials: "include",
-          body: JSON.stringify({ bookId: bookId }),
-        }
-      );
-      if (!response.ok) {
-          throw new Error(`Could not add book`); 
-      }
-    } catch (error) {
-      alert(error);
+export const addExistingBookHandler = async (userId: number, accessToken: string, bookId: string) => {
+  try {
+    const response = await fetch(`https://bookshelf-nou0.onrender.com/api/v1/users/${userId}/add-book`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+      credentials: "include",
+      body: JSON.stringify({ bookId: bookId }),
+    });
+    if (!response.ok) {
+      throw new Error(`Could not add book`);
     }
-  };
+  } catch (error) {
+    alert(error);
+  }
+};
